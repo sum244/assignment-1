360d044c7f88071eb758690396da5806f3c2ab67
sum244 <sumy2244@gmail.com>
2024-01-16 22:36:15 -0500

    Import Request and Response from express

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,7 +1,6 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
-import * as express from 'express';
 import {Request, Response} from 'express';
 
 export default (req: Request, res: Response) => {
