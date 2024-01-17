b691a6fb203891758476b18a43eff8d3ebc05c27
sum244 <sumy2244@gmail.com>
2024-01-16 20:20:24 -0500

    Infer parameter types

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,7 +2,7 @@
 
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: { params: { num: any; }; }, res: { send: (arg0: string) => void; }) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
